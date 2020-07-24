3191fb3d31831a2ac3aeaa6174dbff337101dc78
joel saarinen <40738921+joeliosa@users.noreply.github.com>
2020-07-24 08:59:30 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, request
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+app = Flask(__name__, static_folder="/Users/joelsaarinen/projects/spotifywa/static")
+import spotifyfunctions
+
+chosensongs = []
+
+@app.route("/", methods=["POST", "GET"])
+def home():
+  #first type of POST request; search for artist top songs
+  if request.method == "POST" and not "addbutton" in request.form:
+    rawformdata = (request.form).values()
+    value_iterator = iter(rawformdata)
+    convformdata = next(value_iterator)
+    client_id = '85725aa023e545729e6630b2a369cf90'
+    client_secret = '1abf0e31be774f2d9fb434adb538ae87'
+    spotify = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id, client_secret))
+    searchbynameresult = spotify.search(q='artist:' + convformdata, type='artist')
+    artistitems = searchbynameresult['artists']['items']
+    artist = artistitems[0]
+    artist_uri = artist['uri']
+    trackresults = spotify.artist_top_tracks(artist_uri)
+    return render_template("artistsearch.html", results=trackresults)
+  elif request.method == "POST" and "addbutton" in request.form:
+    rawformdata = (request.form).values()
+    value_iterator = iter(rawformdata)
+    songId = next(value_iterator)
+    trackurl = 'https://api.spotify.com/v1/tracks/' + songId
+    #commented out lines below were me trying to implement playlist display with audio player
+    #client_id = '85725aa023e545729e6630b2a369cf90'
+    #client_secret = '1abf0e31be774f2d9fb434adb538ae87'
+    #spotify = spotipy.Spotify(auth_manager=SpotifyClientCredentials(client_id, client_secret))
+    #relevanttrack = spotifyfunctions.get_track(songId)
+    chosensongs.append(trackurl)
+    return render_template("playlistdisplay.html", playlist=chosensongs)
+  else:
+    return render_template("index.html")
+
+if __name__ == "__main__":
+  app.run(debug=True)
