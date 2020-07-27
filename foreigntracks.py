f34962d0618e28871d608965fcd85890d87308a4
Ali Mian <ali.najam1999@gmail.com>
2020-07-27 13:15:34 +0500

    Methods for finding foreign tracks by country and getting albums

--- foreigntracks.py
+++ foreigntracks.py
@@ -5,19 +5,43 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
-foreigntracks = []
-playliststochoosefrom = [] 
 client_id = '85725aa023e545729e6630b2a369cf90'
 client_secret = '1abf0e31be774f2d9fb434adb538ae87'
-birdy_uri = 'spotify:artist:2WX2uTcsvV5OnS0inACecP'
-spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(client_id, client_secret))
-rawresults = spotify.search(q='playlist:Pakistan', type='playlist', limit=2)
-playlisturls = rawresults['playlists']['items']
-for i in range(0, len(playlisturls)):
-    playliststochoosefrom.append(playlisturls[i]['id'])
-for playlist in playliststochoosefrom:
-    tracksfromplaylist = spotify.playlist_tracks(playlist, limit=3)
-    for i in range(0, len(playliststochoosefrom)):
-        foreigntracks.append(tracksfromplaylist['items'][i]['track']['id'])
-print(foreigntracks)
+sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(client_id, client_secret))
+
+
+
+foreigntracks = []
+playliststochoosefrom = []
+country = ''
+name = ''
+
+def findforeigntracks(country):
+    rawresults = sp.search(q='playlist:country', type='playlist', limit=2)
+    playlisturls = rawresults['playlists']['items']
+    for i in range(0, len(playlisturls)):
+        playliststochoosefrom.append(playlisturls[i]['id'])
+    for playlist in playliststochoosefrom:
+        tracksfromplaylist = sp.playlist_tracks(playlist, limit=3)
+        for i in range(0, len(playliststochoosefrom)):
+            foreigntracks.append(tracksfromplaylist['items'][i]['track']['id'])
+    print(foreigntracks)
+
+findforeigntracks('India')
+
+def getAlbums(name):
+    result = sp.search(q='artist:' + name)  # search query
+    result['tracks']['items'][0]['artists']
+    artist_uri = result['tracks']['items'][0]['artists'][0]['uri']
+    sp_albums = sp.artist_albums(artist_uri, album_type='album')
+    album_names = []
+    album_uris = []
+    for i in range(len(sp_albums['items'])):
+        album_names.append(sp_albums['items'][i]['name'])
+        album_uris.append(sp_albums['items'][i]['uri'])
+    print(album_uris)
+    print(album_names)
+
+
+getAlbums('the Strokes')
 
